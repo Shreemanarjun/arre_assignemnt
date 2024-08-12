75e8ada62797d6e445e7e9be362f4dea6c71920d
Shreeman Arjun Sahu <shreemanarjunsahu@gmail.com>
2024-08-13 03:23:03 +0530

    🔖 update workflow main.dart

--- lib/main_development.dart
+++
@@ -1,6 +0,0 @@
-
-/// This entry point should be used for development only
-void main() {
-  ///You can override your environment variable in bootstrap method here for providers
-  // bootstrap(() => const App());
-}
